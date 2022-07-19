bcaf0a159d669a569f330dea116637f051a24196
shuai_yu <shuai_yu@jit.com.cn>
2022-07-19 17:27:41 +0800

    Mysql.sh docker启动命令更新

--- summary/docker/mysql.sh
+++ summary/docker/mysql.sh
@@ -2,4 +2,24 @@
 #Writerriter by ***
 #docker run mysql
 #2022.6.5
-docker run -d --name docker-mysql-3306 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1qaz@WSX mysql:5.7.38
+docker run -d --name docker-mysql-3306 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1qaz@WSX mysql:5.7.38 --lower_case_table_names=1 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+docker run -d --name docker-mariadb-10.3.35 -p 3308:3306 -e MYSQL_ROOT_PASSWORD=1qaz@WSX --privileged=true -v /opt/docker/mysql/data:/var/lib/mysql  \
+ -v /opt/docker/mysql/my.cnf:/etc/mysql/my.cnf  \
+ mariadb:10.3.35
+
+
+[mysqld]
+skip-name-resolve
+character_set_server=utf8
+datadir=/var/lib/mysql
+server-id=1000
+lower_case_table_names =1
+[mysql]
+default-character-set = utf8
+[mysql.server]
+default-character-set = utf8
+[mysqld_safe]
+default-character-set = utf8
+[client]
+default-character-set = utf8
