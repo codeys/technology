9b9cb5595a100369c8d1924d31cba529182ac938
于帅 <shuai_yu@jit.com.cn>
2022-10-12 14:09:32 +0800

    ApplicationEvent事件监听

--- src/main/java/com/technical/terchnicalsummary/eventpublish/UserEventListener.java
+++ src/main/java/com/technical/terchnicalsummary/eventpublish/UserEventListener.java
@@ -1,23 +1,33 @@
 package com.technical.terchnicalsummary.eventpublish;
 
-import org.springframework.context.ApplicationListener;
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-@Component
-public class UserEventListener implements ApplicationListener<UserEvent> {
+import java.util.Map;
 
-//    @EventListener(classes = {UserEvent.class})
-//    public void userEventListener(UserEvent userEvent) {
-//        System.out.println("userEvent监听：" + userEvent.getName());
-//    }
+@Component
+public class UserEventListener {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+//    @TransactionalEventListener(classes = {UserEvent.class})
+    @EventListener(classes = {UserEvent.class})
+    public void userEventListener(UserEvent userEvent) {
+        String sql = "select * from user";
+        Map<String, Object> stringObjectMap = jdbcTemplate.queryForMap(sql);
+        System.out.println(JSON.toJSONString(stringObjectMap));
+        System.out.println("userEvent监听：" + userEvent.getName());
+    }
 
 //    ApplicationContext applicationContext1 = ApplicationContextUtils.getApplicationContext();
 //    UserEvent userEvent = new UserEvent(new Object());
 //    userEvent.setName("张三");
 //    pplicationContext1.publishEvent(userEvent);
 
-    @Override
-    public void onApplicationEvent(UserEvent userEvent) {
-        System.out.println("userEvent监听：" + userEvent.getName());
-    }
+
+
 }
